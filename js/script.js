b43019a74bfff66062d9ecb1db2e159050239dcb
Flopiras <Flopiras@yahoo.it>
2023-06-01 17:54:37 +0200

    add event prev

--- js/script.js
+++ js/script.js
@@ -26,20 +26,30 @@ const images = document.querySelectorAll('.gallery img');
 //preparo la gestione dell'indice
 let currentIndex = 0;
 
-//impossto la prima immagine come attiva (block)
+//imposto la prima immagine come attiva (block)
 images[currentIndex].classList.add('d-block');
 
 //metto in ascolto il next
 nextButton.addEventListener('click', function (){
 
-//rimuovo la classe block all'immagine con indice corrente
-images[currentIndex].classList.remove('d-block');
+    //rimuovo la classe block all'immagine con indice corrente
+    images[currentIndex].classList.remove('d-block');
    
-//incremento l'indice corrente
-currentIndex++;
+    //incremento l'indice corrente
+    currentIndex++;
 
-//aggiungo la classe block alla nuova immagine corrente
-images[currentIndex].classList.add('d-block');
+    //aggiungo la classe block alla nuova immagine corrente
+    images[currentIndex].classList.add('d-block');
+})
 
+prevButton.addEventListener('click', function (){
 
+    //rimuovo la classe block all'immagine con indice corrente
+    images[currentIndex].classList.remove('d-block');
+   
+    //decremento l'indice corrente
+    currentIndex--;
+
+    //aggiungo la classe block alla nuova immagine corrente
+    images[currentIndex].classList.add('d-block');
 })
