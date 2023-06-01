9abdb4fc12d7d99c55dbd1a4d65da676a38b24e6
Flopiras <Flopiras@yahoo.it>
2023-06-01 17:01:07 +0200

    get DOM elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('js ok')
+
+//Recupero gli elementi dal DOM
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+const gallery = document.querySelector('.gallery');
+
+const sources = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp']
