b83849d4a189517f866bbbd87a614fc11e2e24e1
Flopiras <Flopiras@yahoo.it>
2023-06-01 17:19:26 +0200

    add images in html

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ const gallery = document.querySelector('.gallery');
 const sources = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp']
 
 //preparo un collegamento per l'HTML
-let imageElements = ``;
+let imageElements = '';
 
 //creo un tag img per ogni elemento di sources
 for(let i = 0; i < sources.length; i++){
-    imageElements += `<img scr="${sources[i]}" alt="game${i + 1}">`;
+    imageElements += `<img src="${sources[i]}" alt="game${i + 1}">`;
 }
+
+//inserisco le immagini nel DOM
+gallery.innerHTML = imageElements;
